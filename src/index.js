5ff544e5c37cb1f6709d4a59ae9df9c17b791308
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-02-17 17:26:04 +0100

    Add another todo example

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import projects from "./projects.js";
 import dom from "./dom.js";
 
 console.log("Hello World!");
-const todoExample = {
+const todoExample1 = {
     title: "Lava piatti",
     due_date: "2025-02-18",
     priority: "High",
@@ -12,28 +12,37 @@ const todoExample = {
     notes: "Se non lo faccio vengo sfrattato",
     completed: false,
 }
+const todoExample2 = {
+    title: "Pulisci salotto",
+    due_date: "2025-02-10",
+    priority: "Low",
+    description: "Tutto il salotto deve essere pulito, pure le superfici",
+    notes: "N/A",
+    completed: false,
+}
 
 let newWorkspace = projects.workspace("Jerry");
-console.log("Hi! This is the workspace: ");
-console.log(newWorkspace);
+// console.log("Hi! This is the workspace: ");
+// console.log(newWorkspace);
 
 let newProject = projects.project("Casa");
-console.log("This is the new project: ");
-console.log(newProject)
+// console.log("This is the new project: ");
+// console.log(newProject)
 
-newProject.addTodoToProject(todoExample);
+newProject.addTodoToProject(todoExample1);
+newProject.addTodoToProject(todoExample2);
 newWorkspace.addProject(newProject);
 newWorkspace.addProject(projects.project("Lavoro"));
 
-console.log("This is the workspace with the new project and todo: ");
-console.log(newWorkspace);
+// console.log("This is the workspace with the new project and todo: ");
+// console.log(newWorkspace);
 
 // newWorkspace.deleteProject(0);
 // console.log("This is the workspace after the project was deleted");
 // console.log(newWorkspace);
 
-newWorkspace.projects["0"].todos["0"].updateTitle("Lava piatti in lavatric");
-console.log(newWorkspace.projects["0"].todos["0"]);
+// newWorkspace.projects["0"].todos["0"].updateTitle("Lava piatti in lavatric");
+// console.log(newWorkspace.projects["0"].todos["0"]);
 
 dom.displayProjectsSidebar(newWorkspace.projects);
 dom.displayProjectView(newWorkspace.projects["0"]);
