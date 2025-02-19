c82bfc420c4fa90cfb7ee855c7f2b9178bd0cc0b
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-02-19 17:19:45 +0100

    Add functions to rehydrate objects with their functions from storage

--- src/projects.js
+++ src/projects.js
@@ -40,5 +40,41 @@ const workspace = (newTitle) => {
     )
 }
 
-export default {project, workspace};
+const rehydrateProject = (projectData) => {
+    let idTodo = Math.max(...Object.keys(projectData.todos).map(Number), -1) + 1;
+    return Object.assign(
+        {},
+        projectData,
+        {
+            addChild(newTodo) {
+                this.todos[idTodo] = todos.create(newTodo);
+                newTodo["id"] = idTodo;
+                idTodo += 1;
+            },
+            deleteChild(todoID) {
+                delete this.todos[todoID];
+            }
+        }
+    );
+}
+
+const rehydrateWorkspace = (workspaceData) => {
+    let idProject = Math.max(...Object.keys(workspaceData.projects).map(Number), -1) + 1;
+    return Object.assign(
+        {},
+        workspaceData,
+        {
+            addChild(newProject) {
+                this.projects[idProject] = newProject;
+                newProject["id"] = idProject;
+                idProject += 1;
+            },
+            deleteChild(projectID) {
+                delete this.projects[projectID];
+            }
+        }
+    );
+}
+
+export default {project, workspace, rehydrateProject, rehydrateWorkspace};
 
